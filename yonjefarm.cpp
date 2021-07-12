daf68fb6ca661c3631ba52063715dffcf06bdf76
ATalaei <rererererererererere55@gmail.com>
2021-07-13 00:03:08 +0430

    hopefully last backend for gandomfarm and yonjefarm

--- yonjefarm.cpp
+++ yonjefarm.cpp
@@ -39,40 +39,12 @@ int YonjeFarm::get_level(void){return farm_level;}
 
 bool YonjeFarm::derokardan(void){}
 
-void YonjeFarm::planting(int plantingareawant){}
+//void YonjeFarm::planting(int plantingareawant){}
 // /////////////////////////////////////////////////////////////////////////////////////////////
-void YonjeFarm::upgrade(){
-    if(Data::get_iterator()->get_farm().get_storage().Get_mikh().Get_Number()>=0){
-        //if(Data::get_iterator()->get_farm().get_storage().Get_bill().Get_Number()>=(area*2)){
-        if(Data::get_iterator()->get_coin()>=(5*area)){
-            if(Data::get_iterator()->get_level()>=4){
-                Data::get_iterator()->get_farm().get_storage().Get_mikh().Set_Number(Data::get_iterator()->get_farm().get_storage().Get_mikh().Get_Number()-0);
-                Data::get_iterator()->set_coin(Data::get_iterator()->get_coin()-(5*area));
-                Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()+(3*area));
-                area*=2;
-                //QMessageBox
-                if(Data::get_iterator()->get_experience()>=Data::get_iterator()->get_experience_required_for_levelUp()){
-                    Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()-Data::get_iterator()->get_experience_required_for_levelUp());
-                    Data::get_iterator()->set_experience_required_for_levelUp(2*Data::get_iterator()->get_experience_required_for_levelUp()+10);
-                    //QMessageBox
-                }
-            }
-            else {
-                //QMessageBox
-            }
-        }
-        else {
-            //QMessageBox
-        }
-    }
-    else {
-        //QMessageBox
-    }
 
-}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-bool YonjeFarm::upgradeyonjefarm(int &_nail ,int &_hoe,int &_coin,time_t &_date,int &_playerlevel,int &experience ){}
+//bool YonjeFarm::upgradeyonjefarm(int &_nail ,int &_hoe,int &_coin,time_t &_date,int &_playerlevel,int &experience ){}
 
 //void YonjeFarm:: planting(int plantingareawant){}
 
@@ -90,80 +62,112 @@ void YonjeFarm::on_planting_clicked()
 {
 
    // //////////چک کند زمین شخم زده شده باشد و یک روز گذشته باشد///////////////////////////////////
-    DialogGandomPlanting *d=new DialogGandomPlanting(this) ;
-    connect(d,SIGNAL(plantingareawanting(int)),this,SLOT(plantingareawanted(int _area)));
-    if(areawanted>area){
-        QMessageBox msgBox;
-        msgBox.setText("invalid data( alfalfa farm area equal maximum)");
-        msgBox.exec();
-        areawanted=area;
-    }
+    int i=0;
+    for (i=0;i<1;i++){
+        if(i//for check that is under planting چک کردن برای زیر کشت نبودن//)
+                )
+        {
 
-    d->show();
-    //now i should get gandomnum and decreas number of them.
+                QMessageBox::warning(this,"تاریخ","زمین زیر کشت است. .",QMessageBox::Ok);
+                break;
+    }
+        if (1//زمان چک شود بیش تر از یک روز از شخم زدن گذشته باشد
+                )
+        {
+        QMessageBox::warning(this,"کمبود ضروریات","زمین شخم زده نشده است . .",QMessageBox::Ok);
+        break;
+       }
+        Data::get_iterator()->get_farm().get_storage().Get_yonjeh().Set_Number(Data::get_iterator()->get_farm().get_storage().Get_yonjeh().Get_Number()-area);
+        Data::get_iterator()->get_farm().get_storage().Set_Occupied_Capacity(Data::get_iterator()->get_farm().get_storage().Get_Occupied_Capacity()-area);
+        Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()+(2*area));
+         QMessageBox::information(this,"توجه","کشت انجام شد. .",QMessageBox::Ok);
+        if(Data::get_iterator()->get_experience()>=Data::get_iterator()->get_experience_required_for_levelUp()){
+            Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()-Data::get_iterator()->get_experience_required_for_levelUp());
+            Data::get_iterator()->set_experience_required_for_levelUp(2*Data::get_iterator()->get_experience_required_for_levelUp()+10);
+             QMessageBox::information(this,"تبریک","ارتقای سطح کاربر انجام شد. .",QMessageBox::Ok);
+        }
+     }
 }
 // /////////////////////////////////////////////////////////////////////
-void YonjeFarm::plantingareawanted(int _area)
-{
-    areawanted=_area;
 
-}
-// ///////////////////////////////////////////////////////////////////
-void YonjeFarm::plantingareawantedshokhm(int _area)
-{
-
-areawantedshokhm=_area;
-
-}
 
 // ////////////////////////////////////////////////////////
 void YonjeFarm::on_upgradebutton_clicked()
 {
+    // چک کردن زمان دورئز الزامی است
+    if(Data::get_iterator()->get_level()>=4){
+     if(Data::get_iterator()->get_farm().get_storage().Get_bill().Get_Number()>=(2*area)){
+        if(Data::get_iterator()->get_coin()>=(5*area)){
+                Data::get_iterator()->set_coin(Data::get_iterator()->get_coin()-(5*area));
+                Data::get_iterator()->get_farm().get_storage().Get_bill().Set_Number( Data::get_iterator()->get_farm().get_storage().Get_bill().Get_Number()-2*area);
 
+                Data::get_iterator()->get_farm().get_storage().Get_bill().Set_Number(Data::get_iterator()->get_farm().get_storage().Get_bill().Get_Number()-2*area);
+                Data::get_iterator()->get_farm().get_storage().Set_Occupied_Capacity(Data::get_iterator()->get_farm().get_storage().Get_Occupied_Capacity()-2*area);
+                Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()+(3*area));
+                area*=2;
+                 QMessageBox::information(this,"تبریک","ارتقای مزرعه یونجه انجام شد. .",QMessageBox::Ok);
+                if(Data::get_iterator()->get_experience()>=Data::get_iterator()->get_experience_required_for_levelUp()){
+                    Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()-Data::get_iterator()->get_experience_required_for_levelUp());
+                    Data::get_iterator()->set_experience_required_for_levelUp(2*Data::get_iterator()->get_experience_required_for_levelUp()+10);
+                     QMessageBox::information(this,"تبریک","ارتقای سطح کاربر انجام شد. .",QMessageBox::Ok);
+                }
+
+        }
+        else {
+             QMessageBox::warning(this,"کمبود منابع","سکه به اندازه کافی موجود میست. .",QMessageBox::Ok);
+        }
+    }
+    else {
+        QMessageBox::warning(this,"کمبود منابع","بیل به اندازه کافی موجود میست. .",QMessageBox::Ok);
+    }
+
+   }
+    else {
+       QMessageBox::warning(this,"پایین بودن سطح","به سطح مرد نیاز نرسیده اید. .",QMessageBox::Ok);
+    }
 }
 // /////////////////////////////////////////////////////////////////
 
 void YonjeFarm::on_derokardan_clicked()
 {
-    // /
+    ///
 
-    //  /
-    // / ////////////////چک کند چند روز  بعد کاش باشد
+    ///
+    /// ////////////////چک کند د روز بعد کاش باشد
    if (//بله)
          1 ){
-
-    Data::get_iterator()->get_farm().get_storage().Get_yonjeh().Set_Number(Data::get_iterator()->get_farm().get_storage().Get_yonjeh().Get_Number()+2*areawanted);
+      if(Data::get_iterator()->get_farm().get_storage().GetCapasity()-Data::get_iterator()->get_farm().get_storage().Get_Occupied_Capacity()>=2*area){
+    Data::get_iterator()->get_farm().get_storage().Get_yonjeh().Set_Number(Data::get_iterator()->get_farm().get_siloo().Get_gandom().Get_Number()+2*area);
+    Data::get_iterator()->get_farm().get_storage().Set_Occupied_Capacity(Data::get_iterator()->get_farm().get_storage().Get_Occupied_Capacity()+2*area);
     QMessageBox::warning(this,"موفقیت","درو شد. .",QMessageBox::Ok);
-    if(Data::get_iterator()->get_experience()>=Data::get_iterator()->get_experience_required_for_levelUp()){
+    Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()+(2*area));
+   if(Data::get_iterator()->get_experience()>=Data::get_iterator()->get_experience_required_for_levelUp()){
        Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()-Data::get_iterator()->get_experience_required_for_levelUp());
        Data::get_iterator()->set_experience_required_for_levelUp(2*Data::get_iterator()->get_experience_required_for_levelUp()+10);
-       //QMessageBox
+       QMessageBox::information(this,"تبریک","ارتقای سطح کاربر انجام شد. .",QMessageBox::Ok);
      }
    }
    else {
-       QMessageBox::warning(this,"تاریخ","زمین آماده برداشت نیست. .",QMessageBox::Ok);
+       QMessageBox::warning(this,"ظرفیت ","انبار ظرفیت ندارد. .",QMessageBox::Ok);
    }
+   }
+      else {
+       QMessageBox::warning(this,"تاریخ","زمین آماده برداشت نیست. .",QMessageBox::Ok);
+
+      }
 }
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void YonjeFarm::on_shokhmzadan_clicked()
 {
-
-        DialogGandomPlanting *d=new DialogGandomPlanting(this) ;
-        connect(d,SIGNAL(plantingareawanting(int)),this,SLOT(plantingareawantedshokhm(int _area)));
-        if(areawantedshokhm>area){
-           QMessageBox::warning(this,"مساحت","مساحت شخم زدن مساوی بیش ترین مقدار (مساحت زمین). .",QMessageBox::Ok);
-            areawantedshokhm=area;
-          }
-
-if(Data::get_iterator()->get_coin()>=(areawantedshokhm*5)){
-      Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()+areawantedshokhm);
+if(Data::get_iterator()->get_coin()>=(area*5)){
+      Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()+area);
       if(Data::get_iterator()->get_experience()>=Data::get_iterator()->get_experience_required_for_levelUp()){
           Data::get_iterator()->set_experience(Data::get_iterator()->get_experience()-Data::get_iterator()->get_experience_required_for_levelUp());
           Data::get_iterator()->set_experience_required_for_levelUp(2*Data::get_iterator()->get_experience_required_for_levelUp()+10);
-          //QMessageBox
+          QMessageBox::information(this,"تبریک","ارتقای سطح کاربر انجام شد. .",QMessageBox::Ok);
     }
-      // //////////حالا باید زمان شخم زدن ذخیری شود
+      // ////////////////////////////////////////حالا باید زمان شخم زدن ذخیری شود//////////////////////
 }
 else {
     QMessageBox::warning(this,"مالی","سکه کافی ندارید. .",QMessageBox::Ok);
