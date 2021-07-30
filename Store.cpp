58c4a6a0a7108da5616a81a5cd012623eabae542
Mahdi Ghasemi <ghasemimahdi836@gmail.com>
2021-07-29 23:30:12 -0700

    changes on messagebox

--- Store.cpp
+++ Store.cpp
@@ -39,10 +39,10 @@ void Store::Add_Experience_and_Check_Level(int _e){
 
     if(Data::get_iterator()->get_experience() >= Data::get_iterator()->get_experience_required_for_levelUp()){
         Data::get_iterator()->set_level(Data::get_iterator()->get_level()+1);
+        Data::get_iterator()->set_experience_required_for_levelUp(Data::get_iterator()->get_experience_required_for_levelUp()*2+10);
 
         QMessageBox::information(this,"تبریک","سطح شما افزایش پیدا کرد . . .:)",QMessageBox::Ok);
 
-        Data::get_iterator()->set_experience_required_for_levelUp(Data::get_iterator()->get_experience_required_for_levelUp()*2+10);
 
     }
 
@@ -53,11 +53,11 @@ void Store::on_Buy_Gandom_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_coin() < 3){
-        QMessageBox::warning(this,"","تعداد سکه برای خرید کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد سکه برای خرید کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_siloo().GetCapasity() <= Data::get_iterator()->get_farm().get_siloo().Get_Occupied_Capacity()){
-        QMessageBox::warning(this,"","ظرفیت سیلو برای خرید گندم کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","ظرفیت سیلو برای خرید گندم کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -86,7 +86,7 @@ void Store::on_Sell_Gandom_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_farm().get_siloo().Get_gandom().Get_Number() < 1){
-        QMessageBox::warning(this,"","تعداد گندم موجود در سیلو کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد گندم موجود در سیلو کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else {
@@ -118,17 +118,17 @@ void Store::on_Buy_Cow_pbn_clicked()
         return ;
     }
     else if(Data::get_iterator()->get_level() < 4){
-        QMessageBox::warning(this,"","حداقل سطح مورد نیاز برای خرید گاو ، 4 می باشد . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","حداقل سطح مورد نیاز برای خرید گاو ، 4 می باشد . .",QMessageBox::Ok);
         return ;
     }
 
 
     else if(Data::get_iterator()->get_coin() < 70){
-        QMessageBox::warning(this,"","تعداد سکه برای خرید یونجه کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد سکه برای خرید یونجه کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_cowHome().get_capacity() <= Data::get_iterator()->get_farm().get_cowHome().get_stock_animal()){
-        QMessageBox::warning(this,"","ظرفیت گاوداری برای نگه داری گاو کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","ظرفیت گاوداری برای نگه داری گاو کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -160,11 +160,11 @@ void Store::on_Sell_Cow_pbn_clicked()
         return ;
     }
     else if(Data::get_iterator()->get_level() < 4){
-        QMessageBox::warning(this,"","حداقل سطح مورد نیاز برای فروش گاو ، 4 می باشد . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","حداقل سطح مورد نیاز برای فروش گاو ، 4 می باشد . .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_cowHome().get_stock_animal() < 1){
-        QMessageBox::warning(this,"","تعداد گاو های موجود در گاوداری کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد گاو های موجود در گاوداری کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -195,15 +195,15 @@ void Store::on_Buy_Sheep_pbn_clicked()
         return ;
     }
     else if(Data::get_iterator()->get_level() < 6){
-        QMessageBox::warning(this,"","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
         return ;
     }
     if(Data::get_iterator()->get_coin() < 80){
-        QMessageBox::warning(this,"","تعداد سکه برای خرید گوسفند کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد سکه برای خرید گوسفند کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_sheepHome().get_capacity() <= Data::get_iterator()->get_farm().get_sheepHome().get_stock_animal()){
-        QMessageBox::warning(this,"","ظرفیت آغل برای نگه داری گوسفند کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","ظرفیت آغل برای نگه داری گوسفند کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -235,11 +235,11 @@ void Store::on_Sell_Sheep_pbn_clicked()
         return ;
     }
     else if(Data::get_iterator()->get_level() < 6){
-        QMessageBox::warning(this,"","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_sheepHome().get_stock_animal() < 1){
-        QMessageBox::warning(this,"","تعداد گوسفند های موجود در آغل کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد گوسفند های موجود در آغل کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -270,16 +270,16 @@ void Store::on_Buy_Chicken_pbn_clicked()
         return ;
     }
     else if(Data::get_iterator()->get_level() < 2){
-        QMessageBox::warning(this,"","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
         return ;
     }
 
     else if(Data::get_iterator()->get_coin() < 20){
-        QMessageBox::warning(this,"","تعداد سکه برای خرید مرغ کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد سکه برای خرید مرغ کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_chickenHome().get_capacity() <= Data::get_iterator()->get_farm().get_chickenHome().get_stock_animal()){
-        QMessageBox::warning(this,"","ظرفیت مرغداری برای نگه داری مرغ کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","ظرفیت مرغداری برای نگه داری مرغ کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -310,12 +310,12 @@ void Store::on_Sell_Chicken_pbn_clicked()
         return ;
     }
     if(Data::get_iterator()->get_level() < 2){
-        QMessageBox::warning(this,"","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
         return ;
     }
 
     else if(Data::get_iterator()->get_farm().get_chickenHome().get_stock_animal() < 1){
-        QMessageBox::warning(this,"","تعداد مرغ های موجود در مرغداری کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد مرغ های موجود در مرغداری کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -341,16 +341,16 @@ void Store::on_Buy_Yonjeh_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_level() < 3){
-        QMessageBox::warning(this,"","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
         return ;
     }
 
     else if(Data::get_iterator()->get_coin() < 6){
-        QMessageBox::warning(this,"","تعداد سکه برای خرید یونجه کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد سکه برای خرید یونجه کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_storage().GetCapasity() <= Data::get_iterator()->get_farm().get_storage().Get_Occupied_Capacity()){
-        QMessageBox::warning(this,"","ظرفیت انبار برای خرید یونجه کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","ظرفیت انبار برای خرید یونجه کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -380,11 +380,11 @@ void Store::on_Sell_Yonjeh_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_level() < 3){
-        QMessageBox::warning(this,"","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_storage().Get_yonjeh().Get_Number() < 1){
-        QMessageBox::warning(this,"","تعداد یونجه موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد یونجه موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else {
@@ -412,7 +412,7 @@ void Store::on_Sell_Milk_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_farm().get_storage().Get_milk().Get_Number() < 1){
-        QMessageBox::warning(this,"","تعداد شیر های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد شیر های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else {
@@ -439,11 +439,11 @@ void Store::on_Sell_Wool_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_level() < 6){
-        QMessageBox::warning(this,"","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_storage().Get_pashm().Get_Number() < 1){
-        QMessageBox::warning(this,"","تعداد پشم گوسفند های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد پشم گوسفند های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else {
@@ -470,11 +470,11 @@ void Store::on_Sell_Egg_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_level() < 2){
-        QMessageBox::warning(this,"","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","اين مرحله هنوز براي شما قفل است . .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_storage().Get_egg().Get_Number() < 1){
-        QMessageBox::warning(this,"","تعداد تخم مرغ های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد تخم مرغ های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else {
@@ -500,11 +500,11 @@ void Store::on_Buy_Bill_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_coin() < 50){
-        QMessageBox::warning(this,"","تعداد سکه برای خرید بیل کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد سکه برای خرید بیل کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_storage().GetCapasity() <= Data::get_iterator()->get_farm().get_storage().Get_Occupied_Capacity()){
-        QMessageBox::warning(this,"","ظرفیت انبار برای خرید بیل کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","ظرفیت انبار برای خرید بیل کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -533,7 +533,7 @@ void Store::on_Sell_Bill_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_farm().get_storage().Get_bill().Get_Number() < 1){
-        QMessageBox::warning(this,"","تعداد بیل های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد بیل های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else {
@@ -559,11 +559,11 @@ void Store::on_Buy_Mikh_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_coin() < 30){
-        QMessageBox::warning(this,"","تعداد سکه برای خرید ميخ کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد سکه برای خرید ميخ کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else if(Data::get_iterator()->get_farm().get_storage().GetCapasity() <= Data::get_iterator()->get_farm().get_storage().Get_Occupied_Capacity()){
-        QMessageBox::warning(this,"","ظرفیت انبار برای خرید ميخ کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","ظرفیت انبار برای خرید ميخ کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
 
@@ -592,7 +592,7 @@ void Store::on_Sell_Mikh_pbn_clicked()
 {
 
     if(Data::get_iterator()->get_farm().get_storage().Get_mikh().Get_Number() < 1){
-        QMessageBox::warning(this,"","تعداد ميخ های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
+        QMessageBox::warning(this,"تذکر","تعداد ميخ های موجود در انبار کافی نمی باشد .",QMessageBox::Ok);
         return ;
     }
     else {
